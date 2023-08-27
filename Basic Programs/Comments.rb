167b6706403d9410bbdf7cffb0f943a35dd16fe8
Madhurima Rawat <105432776+madhurimarawat@users.noreply.github.com>
2023-08-27 15:40:05 +0530

    Add files via upload

---
+++ Basic Programs/Comments.rb
@@ -0,0 +1,13 @@
+# # is used for single line comments in ruby
+
+=begin
+is used for multiline comments in ruby
+=end
+
+puts "Comments in Ruby"
+
+puts "Comments are used to make code more informative and easily understandable"
+
+puts "# is used for single line comment"
+
+puts "=begin  =end  all lines in this block are commented out,this is used for multiline comments"
